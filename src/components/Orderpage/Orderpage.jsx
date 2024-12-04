d48dd0ff2de1ca6e153df608592c0aa4e0a02ec4
kathiravan2002 <kathiravankathir910@gmail.com>
2024-12-04 18:45:48 +0530

    order page changes

--- src/components/Orderpage/Orderpage.jsx
+++ src/components/Orderpage/Orderpage.jsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 import React from 'react'
 import Orderheader from '../../shared/components/Orderheader'
-import Orderempty from '../../shared/components/Orderempty'
-import OrderFilter from '../../shared/components/Orderfilter'
+
 
 function Orderpage() {
   return (
@@ -18,4 +15,3 @@ function Orderpage() {
 }
 
 export default Orderpage;
->>>>>>> 237c4fed8500e80ef0fdfcd3c02825b4912b8615
