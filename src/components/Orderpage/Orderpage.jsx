8c6b4603c810e581c7ed71fc0a32c0247ac4b6f9
kathiravan2002 <kathiravankathir910@gmail.com>
2024-12-04 17:58:57 +0530

    changes

--- src/components/Orderpage/Orderpage.jsx
+++ src/components/Orderpage/Orderpage.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import Orderheader from '../../shared/components/Orderheader'
-import Orderempty from '../../shared/components/Orderempty'
-import OrderFilter from '../../shared/components/Orderfilter'
 
 function Orderpage() {
   return (
     <div>
-          <Orderheader />
-          <OrderFilter/>
-          <Orderempty/>
-
-
+      <Orderheader />
     </div>
   )
 }
