1d84e4d244fff5685b9ad14c85f2be54d7b1563e
sathishtcz <msathish.tcz@gmail.com>
2025-04-26 13:52:18 +0530

    26.4.25

--- src/shared/services/Apiendpoint/Apiendpoint.js
+++ src/shared/services/Apiendpoint/Apiendpoint.js
@@ -1,6 +1,6 @@
 const apiurl=()=>{
     // var url="http://192.168.29.71:5000";
-    var url="http://192.168.29.248:5000";
+    var url="http://192.168.29.12:5000";
     return url;
 }
 export default apiurl;
